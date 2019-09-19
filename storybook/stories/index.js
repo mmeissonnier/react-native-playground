ed151163671a8e2d3b40f7409e45c710c3f4ef3a
Mathieu Meissonnier <mmeissonnier@gmail.com>
2019-09-19 22:28:22 +0200

    Fix lint errors

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -5,12 +5,13 @@ import { storiesOf } from '@storybook/react-native';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
-// eslint-disable-next-line import/extensions
 import Button from './Button';
 import CenterView from './CenterView';
 import Welcome from './Welcome';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => (
+  <Welcome showApp={linkTo('Button')} />
+));
 
 storiesOf('Button', module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
