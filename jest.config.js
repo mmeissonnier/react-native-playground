5ca5935ef546fb3d9080607a2d95109e9a5a8ca6
Mathieu Meissonnier <mmeissonnier@gmail.com>
2019-09-20 14:39:41 +0200

    Add lint-staged

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['jest-enzyme'],
+  testEnvironment: 'enzyme',
+  testEnvironmentOptions: {
+    enzymeAdapter: 'react16',
+  },
 };
