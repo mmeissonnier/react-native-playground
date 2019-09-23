1a52c6d1ec73d3ec5d69a7e44294275a484e5f64
Mathieu Meissonnier <mmeissonnier@gmail.com>
2019-09-23 13:12:32 +0200

    Handle Notification Push for Android with FCM

--- android/app/src/main/java/com/myawesomeapptest/MainApplication.java
+++ android/app/src/main/java/com/myawesomeapptest/MainApplication.java
@@ -13,6 +13,8 @@ import com.facebook.soloader.SoLoader;
 
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
 import io.invertase.firebase.firestore.RNFirebaseFirestorePackage;
+import io.invertase.firebase.messaging.RNFirebaseMessagingPackage;
+import io.invertase.firebase.notifications.RNFirebaseNotificationsPackage;
 
 import java.util.List;
 
@@ -32,6 +34,8 @@ public class MainApplication extends Application implements ReactApplication {
       // packages.add(new MyReactNativePackage());
       packages.add(new RNFirebaseAuthPackage());
       packages.add(new RNFirebaseFirestorePackage());
+      packages.add(new RNFirebaseMessagingPackage());
+      packages.add(new RNFirebaseNotificationsPackage());
       return packages;
     }
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createAppContainer } from 'react-navigation';
+import firebase, { firestore } from 'react-native-firebase';
 import Navigator from './components/navigation/Navigators';
 import { useScreens } from 'react-native-screens';
 import { AppContextProvider } from './AppContext';
 import { AppContextInterface, Store } from './types';
+import { Platform } from 'react-native';
 
 useScreens();
 
@@ -19,6 +21,49 @@ const AppContainer = createAppContainer(Navigator);
 const App = () => {
   const [store, updateStore] = useState(defaultStore);
   const storeInterface: AppContextInterface = { store, updateStore };
+  useEffect(() => {
+    let removeNotificationListener: () => void | undefined;
+
+    const checkPermission = async () => {
+      const granted = await firebase.messaging().hasPermission();
+      console.warn('Message Permission : ', granted);
+      if (!granted) {
+        firebase.messaging().requestPermission();
+      } else {
+        // Build a channel
+        const channel = new firebase.notifications.Android.Channel(
+          'test-channel',
+          'Test Channel',
+          firebase.notifications.Android.Importance.Max
+        ).setDescription('My apps test channel');
+
+        // Create the channel
+        firebase.notifications().android.createChannel(channel);
+
+        removeNotificationListener = firebase
+          .notifications()
+          .onNotification(notification => {
+            const headsUpNotification = new firebase.notifications.Notification()
+              .setNotificationId(notification.notificationId)
+              .setTitle(notification.title)
+              .setBody(notification.body)
+              .setData(notification.data);
+            headsUpNotification.android.setChannelId('test-channel');
+            firebase.notifications().displayNotification(headsUpNotification);
+          });
+      }
+    };
+
+    if (Platform.OS === 'android') {
+      checkPermission();
+    }
+
+    return () => {
+      if (removeNotificationListener) {
+        removeNotificationListener();
+      }
+    };
+  }, []);
 
   return (
     <AppContextProvider value={storeInterface}>
